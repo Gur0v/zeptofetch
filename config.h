e2bb9d22ef397e50b59210aab5325f92eed47039
Gurov <103680464+Gur0v@users.noreply.github.com>
2024-12-27 22:53:47 +0000

    Update config.h

--- config.h
+++ config.h
@@ -1,10 +1,11 @@
 #ifndef CONFIG_DEF_H
 #define CONFIG_DEF_H
 
-#define COLOR_RESET "\033[0m"
-#define COLOR_1 "\033[1;34m"
-#define COLOR_2 "\033[1;37m"
-#define COLOR_3 "\033[1;38;5;208m"
+// Default color scheme
+#define COLOR_RESET "\033[0m"     // Reset to terminal default
+#define COLOR_1     "\033[1;34m"  // Bright blue (headers)
+#define COLOR_2     "\033[1;37m"  // Bright white (values)
+#define COLOR_3     "\033[1;38;5;208m"  // Bright orange (accents)
 
 #define BUFFER_SIZE 256
 #define PROC_PATH_FORMAT "/proc/%d/exe"
