5dd66c02fc6c88485c0e45ccc350a20bb4b894d5
Gurov <103680464+Gur0v@users.noreply.github.com>
2025-02-25 13:01:36 +0200

    Delete ui.h
    Merging

--- ui.h
+++
@@ -1,7 +0,0 @@
-#ifndef UI_H
-#define UI_H
-
-void print_ui(const char *username, const char *hostname, const char *distro,
-              const char *kernel, const char *shell, const char *wm, const char *terminal);
-
-#endif
