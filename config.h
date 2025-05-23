7c412f760bb8191d0e098cfd2b557f051b16a81b
Gurov <103680464+Gur0v@users.noreply.github.com>
2025-05-23 18:19:34 +0300

    Update config.h

--- config.h
+++ config.h
@@ -1,9 +1,12 @@
 #ifndef CONFIG_DEF_H
 #define CONFIG_DEF_H
-#define COLOR_RESET "\033[0m"
-#define COLOR_1     "\033[1;34m"
-#define COLOR_2     "\033[1;37m"
-#define COLOR_3     "\033[1;38;5;208m"
-#define BUFFER_SIZE 256
+
+#define COLOR_RESET     "\033[0m"
+#define COLOR_1         "\033[1;34m"
+#define COLOR_2         "\033[1;37m"
+#define COLOR_3         "\033[1;38;5;208m"
+#define BUFFER_SIZE     512
+#define PATH_MAX        4096
 #define PROC_PATH_FORMAT "/proc/%d/exe"
+
 #endif
