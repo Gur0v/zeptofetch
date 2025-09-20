8cf888457b9522e85ba38fcca34dfbc27d3bda82
Gurov <103680464+Gur0v@users.noreply.github.com>
2025-09-20 15:12:38 +0300

    Update config.h

--- config.h
+++ config.h
@@ -1,10 +1,9 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-// Terminal color codes
 #define COLOR_RESET     "\033[0m"
 #define COLOR_1         "\033[1;34m"       // Blue
 #define COLOR_2         "\033[1;37m"       // White
 #define COLOR_3         "\033[1;38;5;208m" // Orange (256-color)
 
-#endif // CONFIG_H
+#endif
