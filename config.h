60e89324d7d67d80ba30be2fab56d84b6771efb8
Gurov <103680464+Gur0v@users.noreply.github.com>
2025-09-20 15:37:02 +0300

    Update config.h

--- config.h
+++ config.h
@@ -2,8 +2,8 @@
 #define CONFIG_H
 
 #define COLOR_RESET     "\033[0m"
-#define COLOR_1         "\033[1;34m"       // Blue
-#define COLOR_2         "\033[1;37m"       // White
-#define COLOR_3         "\033[1;38;5;208m" // Orange (256-color)
+#define COLOR_1         "\033[1;34m"
+#define COLOR_2         "\033[1;37m"
+#define COLOR_3         "\033[1;38;5;208m"
 
 #endif
