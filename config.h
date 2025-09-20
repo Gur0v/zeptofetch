34d7cca0ad91744eeeaac841a05b573f119285a2
Gurov <103680464+Gur0v@users.noreply.github.com>
2025-09-20 15:09:30 +0300

    Update config.h

--- config.h
+++ config.h
@@ -1,12 +1,18 @@
-#ifndef CONFIG_DEF_H
-#define CONFIG_DEF_H
+#ifndef CONFIG_H
+#define CONFIG_H
 
+// Terminal color codes
 #define COLOR_RESET     "\033[0m"
-#define COLOR_1         "\033[1;34m"
-#define COLOR_2         "\033[1;37m"
-#define COLOR_3         "\033[1;38;5;208m"
-#define BUFFER_SIZE     512
+#define COLOR_1         "\033[1;34m"       // Blue
+#define COLOR_2         "\033[1;37m"       // White
+#define COLOR_3         "\033[1;38;5;208m" // Orange (256-color)
+
+// Fallback max path length if not defined by system
+#ifndef PATH_MAX
 #define PATH_MAX        4096
+#endif
+
+// Format string for /proc/<pid>/exe
 #define PROC_PATH_FORMAT "/proc/%d/exe"
 
-#endif
+#endif // CONFIG_H
