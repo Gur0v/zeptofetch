6a669268e23238c55fa1a9e45d3c5af44ac80d1e
Gurov <103680464+Gur0v@users.noreply.github.com>
2025-09-20 15:11:10 +0300

    Update config.h

--- config.h
+++ config.h
@@ -7,12 +7,4 @@
 #define COLOR_2         "\033[1;37m"       // White
 #define COLOR_3         "\033[1;38;5;208m" // Orange (256-color)
 
-// Fallback max path length if not defined by system
-#ifndef PATH_MAX
-#define PATH_MAX        4096
-#endif
-
-// Format string for /proc/<pid>/exe
-#define PROC_PATH_FORMAT "/proc/%d/exe"
-
 #endif // CONFIG_H
