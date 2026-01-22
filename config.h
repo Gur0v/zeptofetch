0987c0f58e7efaa6be841353baeb5903f8696686
Gurov <gurov@disroot.org>
2026-01-22 17:00:20 +0200

    Edit config.h

--- config.h
+++ config.h
@@ -1,9 +1,9 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define COLOR_RESET     "\033[0m"
-#define COLOR_1         "\033[1;34m"
-#define COLOR_2         "\033[1;37m"
-#define COLOR_3         "\033[1;38;5;208m"
+#define CR      "\033[0m"
+#define C1      "\033[1;34m"
+#define C2      "\033[1;37m"
+#define C3      "\033[1;38;5;208m"
 
 #endif
